5449b26865b89a1640673fd1421188123d41c5ec
Jacquie Yu <42724574+JacquieLeeYu@users.noreply.github.com>
2018-11-08 12:40:24 -0500

    Add files via upload

---
+++ ChatFilter.java
@@ -0,0 +1,20 @@
+/**
+ * CS18000 Project 4 - Simple Chat Server
+ *
+ *
+ *
+ * @author Jacquie Yu, yu790@purdue.edu & Siddarth Pillai
+ *
+ * @version November
+ */
+
+public class ChatFilter {
+
+    public ChatFilter(String badWordsFileName) {
+
+    }
+
+    public String filter(String msg) {
+        return msg;
+    }
+}
---
+++ ChatMessage.java
@@ -0,0 +1,19 @@
+/**
+ * CS18000 Project 4 - Simple Chat Server
+ *
+ *
+ *
+ * @author Jacquie Yu, yu790@purdue.edu & Siddarth Pillai
+ *
+ * @version November
+ */
+
+
+import java.io.Serializable;
+
+final class ChatMessage implements Serializable {
+    private static final long serialVersionUID = 6898543889087L;
+
+    // Here is where you should implement the chat message object.
+    // Variables, Constructors, Methods, etc.
+}
